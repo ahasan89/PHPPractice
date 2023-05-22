2ee90adbf3763110b7d1ef5170d04e83dbd629c0
User <mahasan89live@gmail.com>
2023-05-22 20:24:20 +0600

    1st update test

---
+++ basicLoop.php
@@ -0,0 +1,37 @@
+<?php
+
+$n=50;
+$sumOdd=0;
+$sumEven=0;
+
+$numbers=0;
+$i=1;
+for($i=1;$i<=$n;$i++){
+if($i%2!=0){
+
+$sumOdd+=$i;
+
+
+
+echo "Series of Odd number between 1 to 50=".$i." <br>";
+
+
+$numbers=[$i];
+
+}elseif($i%2==0){
+
+$sumEven+=$i;
+
+echo "Series of Even number between 1 to 50=".$i." <br>";
+}
+
+
+
+}
+
+echo "Numbers of odd numbers in Series is =".count($numbers);
+echo "<br>Sum of Series of Odd number between 1 to 50=".$sumOdd;
+
+echo "<br>Sum of Series of Even number between 1 to 50=".$sumEven;
+
+?>
---
+++ namePrint.php
@@ -0,0 +1,44 @@
+<?php
+
+for($i=1;$i<=10;$i++){
+
+echo $i.". "."Abu Hasan<br>";
+
+}
+
+//if(isset($_POST['save'])){
+//$fi=$_POST['first'];
+//$si=$_POST['second'];
+//if($fi>0 && $si<10){
+//echo strval($fi);echo strval($fi);
+//
+//}
+//
+//}
+
+?>
+<?php /*?><br><br>
+<html lang="en" xml:lang="" dir="" >
+<head>
+<meta content="" />
+<script>
+
+</script>
+
+</head>
+<body>
+<div style="400px">
+<div>INput1: <input id="first" name="first" type="number" /></div>
+<div>INput2: <input id="second" name="second" type="number" /></div>
+<div align="">&nbsp;&nbsp;&nbsp;&nbsp;<input id="save" name="save" type="submit"  value="Submit"/>
+<? echo strval($fi);echo strval($fi);?>
+</div>
+
+</div>
+
+</body>
+
+<footer>
+&copy;copy Reserved.
+</footer>
+</html><?php */?>
